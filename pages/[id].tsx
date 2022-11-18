cb201d1b6639726945ecc8d0d6aceeaf75a7bc7c
Zubair Sheikh <izubairsh@gmail.com>
2022-11-19 01:11:03 +0500

    added profile template

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Profile.module.css';
 
 export default function Profile() {
   return (
@@ -14,7 +14,70 @@ export default function Profile() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}></main>
+      <main className="">
+        <div className="container mt-4 mb-4 p-3 d-flex justify-content-center">
+          <div className="card p-4">
+            <div className=" image d-flex flex-column justify-content-center align-items-center">
+              <button className="btn btn-secondary">
+                {' '}
+                <Image
+                  src="https://i.imgur.com/wvxPV9S.png"
+                  height="100"
+                  width="100"
+                  alt="test"
+                />
+              </button>{' '}
+              <span className="name mt-3">Eleanor Pena</span>{' '}
+              <span className="idd">@eleanorpena</span>
+              <div className="d-flex flex-row justify-content-center align-items-center gap-2">
+                {' '}
+                <span className="idd1">Oxc4c16a645_b21a</span>{' '}
+                <span>
+                  <i className="fa fa-copy"></i>
+                </span>{' '}
+              </div>
+              <div className="d-flex flex-row justify-content-center align-items-center mt-3">
+                {' '}
+                <span className="number">
+                  1069 <span className="follow">Followers</span>
+                </span>
+              </div>
+              <div className=" d-flex mt-2">
+                <button className="btn1 btn-dark">Edit Profile</button>
+              </div>
+              <div className="text mt-3">
+                {' '}
+                <span>
+                  Eleanor Pena is a creator of minimalistic x bold graphics and
+                  digital artwork.
+                  <br />
+                  <br /> Artist/ Creative Director by Day #NFT minting@ with FND
+                  night.{' '}
+                </span>{' '}
+              </div>
+              <div className="gap-3 mt-3 icons d-flex flex-row justify-content-center align-items-center">
+                {' '}
+                <span>
+                  <i className="fa fa-twitter"></i>
+                </span>{' '}
+                <span>
+                  <i className="fa fa-facebook-f"></i>
+                </span>{' '}
+                <span>
+                  <i className="fa fa-instagram"></i>
+                </span>{' '}
+                <span>
+                  <i className="fa fa-linkedin"></i>
+                </span>{' '}
+              </div>
+              <div className=" px-2 rounded mt-4 date ">
+                {' '}
+                <span className="join">Joined May,2021</span>{' '}
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
 
       <footer className={styles.footer}></footer>
     </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,32 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Script from 'next/script';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+          rel="stylesheet"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <Script
+        id="bootstrap-cdn"
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+      />
+      <Script
+        id="bootstrap"
+        src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
